a019a55f3a357a19bce0fb3092e38813c98b6f69
akshajkhanduri87 <akshaj.khanduri@gmail.com>
2025-06-24 20:53:47 -0700

    calc

---
+++ projects/simple_calc.py
@@ -0,0 +1,13 @@
+# 6/24/25
+
+def calculator(a,b,operation):
+    if operation == "+":
+        print(a+b)
+    elif operation == "-":
+        print(a-b)
+    elif operation == "*":
+        print(a*b)
+    elif operation == "/":
+        print(a/b)
+
+calculator(20,5,"+")
