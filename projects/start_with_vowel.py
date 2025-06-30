d195687e71faac650229a465542e4a8088dfff7e
akshajkhanduri87 <akshaj.khanduri@gmail.com>
2025-06-30 09:36:31 -0700

    vowelstart

---
+++ projects/start_with_vowel.py
@@ -0,0 +1,9 @@
+# 6/30/25
+
+def starts_with_vowel(word):
+    if word[0] == "a" or word[0] == "e" or word[0] == "i" or word[0] == "o" or word[0] == "u":
+        print("True")
+    else:
+        print("False")
+
+starts_with_vowel("elephant")
