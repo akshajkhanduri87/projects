d53ee85f44e46a6d346e223558adea11b3ae9517
akshajkhanduri87 <akshaj.khanduri@gmail.com>
2025-07-03 15:50:06 -0700

    wordinsentence

---
+++ projects/is_word_in_sentence.py
@@ -0,0 +1,10 @@
+# 7/3/25
+
+
+def word_in_sentence(word,sentence):
+    if word in sentence:
+        print("That word is in the sentence")
+    else:
+        print("Not in sentence")
+
+word_in_sentence("hello","hello my name is Jack")
