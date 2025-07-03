b4e4a86754a6ea577a44029e1102e601db444dec
akshajkhanduri87 <akshaj.khanduri@gmail.com>
2025-07-03 15:36:21 -0700

    firstandlast

---
+++ projects/first_and_last_letter_same.py
@@ -0,0 +1,9 @@
+# 7/3/25
+
+def first_last_same(word):
+    if word[0] == word[-1]:
+        print("First and last letter same")
+    else:
+        print("First and last letter are not same")
+
+first_last_same("radar")
