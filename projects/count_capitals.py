33207fce8064724b57bc267f87c26ab03fdbdd31
akshajkhanduri87 <akshaj.khanduri@gmail.com>
2025-08-05 15:15:30 -0700

    8/5/25

---
+++ projects/count_capitals.py
@@ -0,0 +1,10 @@
+# 8/5/25
+def count_capitals(word):
+    capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    number_of_capitals = 0
+    for letter in word:
+        if letter in capitals:
+            number_of_capitals += 1
+    print(number_of_capitals)
+
+count_capitals("HellOThereE")
