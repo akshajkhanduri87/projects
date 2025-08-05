03ac3c9bdde15a27a4e257f7472437172102fb76
akshajkhanduri87 <akshaj.khanduri@gmail.com>
2025-08-05 15:07:39 -0700

    8/5/25

---
+++ projects/two_lists_common.py
@@ -0,0 +1,9 @@
+# 8/5/25    
+def common_elements_in_lst(lst1,lst2):
+    common_elements = []
+    for item in lst1:
+        if item in lst2 and item not in common_elements:
+            common_elements.append(item)
+    print(common_elements)
+
+common_elements_in_lst([1,2,3],[1,2,3,4])
